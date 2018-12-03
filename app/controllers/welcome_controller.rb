83ce5604d3e7c8e43dd3f4327543c6105306e4dd
shadow_boxer81 <kgbarayor@envisagelabs.com>
2018-12-03 12:36:37 -0800

    Make app production ready

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,4 +6,29 @@ class WelcomeController < ApplicationController
   def about
 
   end
+
+  def sbtop
+
+  end
+
+  def graphics
+
+  end
+
+
+  def test_flash
+
+  end
+
+
+  def bulk_push
+    begin
+      User.send_bulk_notifications(params[:message])
+      redirect_to admin_notification_path, notice: "Insufficient rights!"
+    rescue
+      redirect_to admin_notification_path, alert: "Error"
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  resources :users
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+  get 'sbtop', to: 'welcome#sbtop'
+  get 'graphics', to: 'welcome#graphics'
+  get 'last24', to: 'welcome#last24'
+  get 'cnc', to: 'welcome#cnc'
+  get 'cnc_accepted', to: 'welcome#cnc_accepted'
+  get 'test_flash', to: 'welcome#test_flash'
+
+
+  get 'login',to:'sessions#new'
+  post 'login',to:'sessions#create'
+  delete 'logout',to:'sessions#destroy'
 end
